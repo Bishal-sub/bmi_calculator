00e139ebdb71dba3362061879c2213696f3d98f1
Bishal <173034130+Bishal-sub@users.noreply.github.com>
2025-01-25 08:38:28 +0545

    Update splashscreen.dart

--- lib/screen/splashscreen.dart
+++ lib/screen/splashscreen.dart
@@ -1,1 +1,43 @@
+import 'dart:async';
+
+import 'package:bmi_calculator/screen/home.dart';
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 2),
+      () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => Home(),
+      )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 150,
+              child: Image.asset("assets/bmi.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
