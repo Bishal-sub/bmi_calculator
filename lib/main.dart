43a16aec22fdba31941c6e42774fcd699a2998c2
Bishal <173034130+Bishal-sub@users.noreply.github.com>
2025-01-25 08:39:28 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,1 @@
-import 'package:bmi_calculator/screen/splashscreen.dart';
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Splashscreen(),
-    );
-  }
-}
